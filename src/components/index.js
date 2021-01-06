840b64879b8703144b4b7bd079708ea4217eae4a
Toma Rosa <rosa.toma@outlook.fr>
2021-01-06 14:48:48 +0100

    Déplacement de la ref. de App dans l'index.js du src.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as App } from "../App";
 export { default as Header } from "./Header";
 export { default as Logo } from "./Logo";
 export { default as Section } from "./Section";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import { App } from "components";
+import { App } from "App";
 import { MapContextProvider } from "context/MapContext";
 
 ReactDOM.render(
