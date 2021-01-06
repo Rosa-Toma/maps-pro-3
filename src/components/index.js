e208b363388efdfa4ab43f01ea4d7c576bb62c11
Toma Rosa <rosa.toma@outlook.fr>
2021-01-06 14:37:53 +0100

    Déplacement du dossier App à la racine du src.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as App } from "./App";
+export { default as App } from "../App";
 export { default as Header } from "./Header";
 export { default as Logo } from "./Logo";
 export { default as Section } from "./Section";
